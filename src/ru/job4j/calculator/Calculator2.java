45d28a8c91bcc6337289f21f6c43c504a55e4b19
Vik <dr.john.zoidberg.1@gmail.com>
2021-08-15 23:22:34 +0300

    4.2. Результат работы метода [#232622]

---
+++ src/ru/job4j/calculator/Calculator2.java
@@ -0,0 +1,12 @@
+package ru.job4j.calculator;
+
+public class Calculator2 {
+    public static void main(String[] args) {
+        int size = 10;
+            System.out.println(size);
+        size = 100;
+            System.out.println(size);
+        size = size - 5;
+            System.out.println(size);
+    }
+}
---
+++ src/ru/job4j/calculator/Calculator3.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculator;
+
+public class Calculator3 {
+        public static void plus (int first, int second) {
+            int result = first + second;
+            System.out.println(result);
+        }
+
+        public static void main(String[] args) {
+            Calculator3.plus(100, 500);
+            Calculator3.plus(4, 2);
+            Calculator3.plus(3, 5);
+        }
+}

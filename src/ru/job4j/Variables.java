37abcbb1e2be47c60e099b49419e11eebb1d1bf5
Vik <dr.john.zoidberg.1@gmail.com>
2021-08-22 19:47:49 +0300

    9.1. String [#237407 #193069] Изм.1

---
+++ src/ru/job4j/Variables.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte age = 28;
+        char gender = 'm'; // gender m/f
+        short height = 1860, bricks = 198; // height - mm; bricks - m^3
+        long bacterias = 186481681154846486L, bites = 1584345L;
+        float spcdist = 11.61F; //million light year
+        boolean work = true;
+
+    }
+}
---
+++ test/ru/job4j/MyTest.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class MyTest {
+    public static void main(String[] args) {
+        char c = 45000;
+        float f = (float) c;
+        System.out.println(f);
+    }
+}

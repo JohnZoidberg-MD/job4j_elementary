9f9bc3e9ddbcd238998f1da46f4b3e72d0ac5fba
Vik <dr.john.zoidberg.1@gmail.com>
2021-08-22 13:46:19 +0300

    4.1. Аргументы [#295521 #190293] Исп.

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -1,17 +1,17 @@
-package ru.job4j;
+package ru.job4j.calculator;
 
 public class ArgMethod {
 
     public static void hello(String name, int age) {
-        System.out.println("Hello, " + name);
-        System.out.println("age = " + age);
         System.out.println("Hello, " + name + ", age = " + age);
-        System.out.println("age = " + age + ", Hello, " + name);
     }
 
     public static void main(String[] args) {
-        String name = "job4j";
+        String name = "Job4j";
         int age = 33;
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
     }
 }
---
+++ src/ru/job4j/condition/SqArea.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    public static double square(double p, double k) {
+        double rsl = 0;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

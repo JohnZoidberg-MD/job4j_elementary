8d54f2b46dbf9f0b1ffbc954ddeff8f78aa57906
Vik <dr.john.zoidberg.1@gmail.com>
2021-08-22 19:48:29 +0300

    9.1. String [#237407 #193069] Изм.1

--- test/ru/job4j/MyTest.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j;
-
-public class MyTest {
-    public static void main(String[] args) {
-        char c = 45000;
-        float f = (float) c;
-        System.out.println(f);
-    }
-}

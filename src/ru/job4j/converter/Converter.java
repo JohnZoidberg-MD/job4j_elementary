1a0cc5de56c91b4b45a37dc5574a0b0a85f877d9
Vik <dr.john.zoidberg.1@gmail.com>
2021-08-22 19:49:26 +0300

    0. Что такое тестирование [#159431]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,22 +1,32 @@
 package ru.job4j.converter;
 
 public class Converter {
-
-    public static float rubToEur(float value) {
-        float rslt = value / 70F;
-        return rslt;
+    public static float rubleToEuro(float value) {
+        return value / 80;
     }
 
-    public static float rubToUsd(float value) {
-        float rslt = value / 60F;
-        return rslt;
+    public static float rublesToDollar(float value) {
+        return value / 75;
     }
 
     public static void main(String[] args) {
-        Float rub = 140F;
-        float eur = Converter.rubToEur(rub);
-        float usd = Converter.rubToUsd(rub);
-        System.out.println(rub + " rubles = " + eur + " euros");
-        System.out.println(rub + " rubles = " + usd + " dollars");
+        float rubles = 180;
+        float euro = Converter.rubleToEuro(rubles);
+        float dollar = Converter.rublesToDollar(rubles);
+        System.out.println(rubles + " rubles = " + euro + " euros");
+        System.out.println(rubles + " rubles = " + dollar + " dollars");
+
+        System.out.println();
+
+        float in = 180;
+        float expectedEuro = 2.25f;
+        float outEuro = Converter.rubleToEuro(in);
+        boolean passedEuro = expectedEuro == outEuro;
+        System.out.println(in + " rubles = " + expectedEuro + ". Test result: " + passedEuro);
+        float expectedDollar = 2.4f;
+        float outDollar = Converter.rublesToDollar(in);
+        boolean passedDollar = expectedDollar == outDollar;
+        System.out.println(in + " rubles = " + expectedDollar + ". Test result " + passedDollar);
     }
 }
+

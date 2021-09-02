948ff7107ff18dda0af4c940c6ac71b14dd13767
Vik <dr.john.zoidberg.1@gmail.com>
2021-09-02 15:47:50 +0300

    4.3.6. Math.abs [#373616 #197712]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -3,12 +3,12 @@ package ru.job4j.condition;
 public class ChessBoard {
 
     public static int way(int x1, int y1, int x2, int y2) {
-        // return Math.abs(x2 - x1) == Math.abs(y2 - y1) ? Math.abs(x2 - x1) : 0;
-       /* Данная формула верна, но тесты  whenX1isMinus1ThenWayIs0 и whenY1isMinus1ThenWayIs0 выдадут ошибку
-       т.к. тесты не верны. Движение фигуры может принимать отрицательные значения (движение назад),
-       но изначальные координаты фигуры НЕ могут быть отрицательными, т.к. координаты шахматной доски положительны.
-       Учитывая что задание нужно выполнить "любой ценой" и цель задания не научиться играть в шахматы,
-       то примем верной следующую формулу: */
-        return Math.abs(Math.abs(x2) - Math.abs(x1)) == Math.abs(Math.abs(y2) - Math.abs(y1)) ? Math.abs(Math.abs(x2) - Math.abs(x1)) : 0;
+        int rsl;
+        if ((x1 >= 0 && x1 <= 7) && (y1 >= 0 && y1 <= 7) && (x2 >= 0 && x2 <= 7) && (y2 >= 0 && y2 <= 7)) {
+            rsl = Math.abs(x2 - x1) == Math.abs(y2 - y1) ? Math.abs(x2 - x1) : 0;
+        } else {
+            rsl = 0;
+        }
+        return rsl;
     }
 }

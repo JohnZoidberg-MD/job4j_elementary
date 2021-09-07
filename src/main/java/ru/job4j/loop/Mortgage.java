baa5a1b4310e4e421e29b8c392c55dd196aa90a5
Vik <dr.john.zoidberg.1@gmail.com>
2021-09-07 15:18:02 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        for (year = 0; amount > 0; year++) {
+            amount = (amount + amount * percent / 100) - salary;
+        }
+        return year;
+    }
+}

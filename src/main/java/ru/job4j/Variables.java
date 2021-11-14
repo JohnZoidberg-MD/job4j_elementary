28c7dd4ff676b8e1bf0af1a68acf91e3db43d68f
Vik <dr.john.zoidberg.1@gmail.com>
2021-11-14 16:26:43 +0300

    Fix

--- src/main/java/ru/job4j/Variables.java
+++ src/main/java/ru/job4j/Variables.java
@@ -3,10 +3,10 @@ package ru.job4j;
 public class Variables {
     public static void main(String[] args) {
         byte age = 28;
-        char gender = 'm'; // gender m/f
-        short height = 1860, bricks = 198; // height - mm; bricks - m^3
+        char gender = 'm';
+        short height = 1860, bricks = 198;
         long bacterias = 186481681154846486L, bites = 1584345L;
-        float spcdist = 11.61F; //million light year
+        float spcdist = 11.61F;
         boolean work = true;
 
     }
